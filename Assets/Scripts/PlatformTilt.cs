f2a6343629d6880925b5802bec25b1b74ed6b7d1
stevendbernas <88916764+stevendbernas@users.noreply.github.com>
2024-11-06 00:35:39 -0800

    second commit

---
+++ Assets/Scripts/PlatformTilt.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlatformTilt : MonoBehaviour
+{
+    public float tiltSpeed = 10f; 
+
+    void Update()
+    {
+        float tiltX = 0f;
+        float tiltZ = 0f;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tiltX = tiltSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tiltX = -tiltSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tiltZ = tiltSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tiltZ = -tiltSpeed * Time.deltaTime;
+        }
+
+        transform.Rotate(tiltX, 0, tiltZ);
+    }
+}
