81f033fae67efdabc648a75b1db4c3995ede84d7
Kenichi Sakamoto <eug.ken0601@gmail.com>
2024-11-10 21:50:15 -0800

    fixed board controller

--- Assets/Scripts/Player/BoardController.cs
+++ Assets/Scripts/Player/BoardController.cs
@@ -11,20 +11,20 @@ public class BoardController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            tiltX = tiltSpeed * Time.deltaTime;
+            tiltX = -tiltSpeed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            tiltX = -tiltSpeed * Time.deltaTime;
+            tiltX = tiltSpeed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            tiltZ = tiltSpeed * Time.deltaTime;
+            tiltZ = -tiltSpeed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            tiltZ = -tiltSpeed * Time.deltaTime;
+            tiltZ = tiltSpeed * Time.deltaTime;
         }
 
         transform.Rotate(tiltX, 0, tiltZ);
