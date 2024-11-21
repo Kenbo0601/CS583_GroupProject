95785442549d1240cbf67fa6df7d22f0c6f8c11c
Kenichi Sakamoto <eug.ken0601@gmail.com>
2024-11-20 23:48:57 -0800

    added score manager

---
+++ Assets/Scripts/GameObjects/Coin.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void OnCollisionEnter(Collision coll) {
+        // Find out if the ball hit the coin
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.CompareTag("Ball")) {
+            // Destroy the object if it has the "Pea" tag
+            Destroy(this.gameObject);
+            // Increase the score
+            ScoreManager.AddScore(100);
+        }
+    }
+}
